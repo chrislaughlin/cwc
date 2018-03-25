434246cedb578b5619e753835cc0df7fcc26b65e
Chris Laughlin <bluntm@gmail.com>
2018-03-25 15:43:27 +0100

    adding index and home templates

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,34 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-import './index.css'
-
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <div>
+        <Helmet
+            title="CWC - Creative Wedding Company"
+            meta={[
+                { name: 'description', content: 'Creative Wedding Company' },
+                { name: 'keywords', content: 'Creative, Wedding, Company' },
+            ]}
+        />
+        <div>
+            LANDING PAGE !!!!!!
+            {children()}
+        </div>
     </div>
-  </div>
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+    children: PropTypes.func,
 }
 
 export default TemplateWrapper
